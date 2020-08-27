3bf6fb72d42f24ed80223c35063e40fbb937ee08
Cameron Eldridge <cameldridge+git@gmail.com>
2020-08-27 00:16:44 -0400

    Arrange some text

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script lang='typescript'>
 </script>
 
+<main>
+  Hello.
+</main>
+
 <style>
+  :global(body) {
+    padding: 0;
+    margin: 0;
+    font: 12pt mono;
+  }
+
+  main {
+    width: 80ch;
+    margin: 0 auto;
+    word-wrap: break-word;
+    padding: 2em 0;
+  }
 </style>
